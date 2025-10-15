9bdc07160a762f1c48f28ea2c0b27742ebb75359
Amirali <a.valikhani1999@gmail.com>
2025-10-15 23:46:45 +0330

    factorial is done

---
+++ factorial/factorial.js
@@ -0,0 +1,12 @@
+let num = Number(prompt("enter number : "));
+
+
+let factorial = 1; 
+
+
+for (let i = 1; i <= num; i++) {
+  factorial = factorial * i;
+}
+
+
+console.log("factorial " + num + " equal = " + factorial);
