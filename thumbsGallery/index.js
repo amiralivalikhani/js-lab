068142d04e1d29647fdbc0b295a7e6fb4b2064d5
Amirali <a.valikhani1999@gmail.com>
2025-10-29 13:03:03 +0330

    thumbs gallery

---
+++ thumbsGallery/index.js
@@ -0,0 +1,33 @@
+
+
+
+function season1() {
+    const image = document.querySelector(".pictiure img");
+    const section = document.querySelector(".gallery img");
+    section.getAttribute("src");
+    image.setAttribute("src", "./imgs/1.jpg")
+}
+
+
+function season2() {
+    const image = document.querySelector(".pictiure img");
+    const section = document.querySelector(".gallery img");
+    section.getAttribute("src");
+    image.setAttribute("src", "./imgs/2.jpg")
+}
+
+
+function season3() {
+    const image = document.querySelector(".pictiure img");
+    const section = document.querySelector(".gallery img");
+    section.getAttribute("src");
+    image.setAttribute("src", "./imgs/3.jpg")
+}
+
+
+function season4() {
+    const image = document.querySelector(".pictiure img");
+    const section = document.querySelector(".gallery img");
+    section.getAttribute("src");
+    image.setAttribute("src", "./imgs/4.jpg")
+}
